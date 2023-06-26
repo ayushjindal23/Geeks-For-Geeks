6663709163ea13abe6805e02b4a9c8941430968b
Ayush Jindal <64908612+ayushjindal23@users.noreply.github.com>
2023-06-25 23:48:38 -0700

    Create Square root of a number.cpp

---
+++ Binary Search/Square root of a number.cpp
@@ -0,0 +1,21 @@
+class Solution{
+  public:
+    long long int floorSqrt(long long int x) 
+    {  
+        int low=0,high = x;
+        int ans=-1;
+        while(low<=high){
+            
+            long long mid = (low+high)/2;
+            
+            if(mid*mid<=x){
+                ans =mid;
+                low=mid+1;
+            }else{
+                high=mid-1;
+            }
+            
+        }
+        return ans;
+    }
+};
