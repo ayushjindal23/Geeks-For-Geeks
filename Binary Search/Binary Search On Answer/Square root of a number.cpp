2450c5201785ee7e3898c9aaa91defe3823278e9
Ayush Jindal <64908612+ayushjindal23@users.noreply.github.com>
2023-06-26 00:32:45 -0700

    Update Square root of a number.cpp

--- Binary Search/Binary Search On Answer/Square root of a number.cpp
+++ Binary Search/Binary Search On Answer/Square root of a number.cpp
@@ -1,24 +1,25 @@
 class Solution{
   public:
     long long int floorSqrt(long long int x) 
-    {  
-        
+    {
+       
         // Base cases
         if (x == 0 || x == 1)
-            return x;
-      
-        int low=0,high = x;
-        int ans=-1;
+            return x; 
+            
+        long long int low=1;
+        long long int high=x;
+        
+        long long int ans;
         while(low<=high){
             
-            long long mid = (low+high)/2;
-          
+            long long int mid = (low+high)/2;
+            
             if (mid*mid == x) return mid;
-          
+
             if(mid*mid<=x){
                 ans =mid;
                 low=mid+1;
-              
             }else{
                 high=mid-1;
             }
