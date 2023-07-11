20ebcea79ec0d1e7e34f278021c8dc85ba9c60a2
Ayush Jindal <64908612+ayushjindal23@users.noreply.github.com>
2023-07-10 20:52:16 -0700

    Update Split Array Largest Sum.cpp

--- Binary Search/Binary Search On Answer/Split Array Largest Sum.cpp
+++ Binary Search/Binary Search On Answer/Split Array Largest Sum.cpp
@@ -1,7 +1,8 @@
 class Solution {
-public:
+  public:
+    
     bool check(int mid, int array[], int n, int K) {
-        int count = 0;
+        int count = 1;
         int sum = 0;
         
         for (int i = 0; i < n; i++) {
@@ -10,12 +11,11 @@ public:
             
             // If the sum is greater than mid, increase count
             if (sum > mid) {
-                count++;
                 sum = array[i];
+                count++;
+
             }
         }
-        count++;
-     
         // Check condition
         if (count <= K)
             return true;
@@ -23,8 +23,10 @@ public:
         return false;
     }
   
-    int splitArray(int arr[], int N, int K) {
-        int* maxElement = max_element(arr, arr + N);
+  
+    int splitArray(int arr[] ,int N, int K) {
+        // code here
+          int* maxElement = max_element(arr, arr + N);
         int start = *maxElement; // Max subarray sum, considering subarray of length 1
         int end = 0;
      
@@ -32,21 +34,18 @@ public:
             end += arr[i]; // Max subarray sum, considering subarray of length N
         }
      
-        // The answer stores the possible maximum subarray sum
-        int answer = 0;
         
         while (start <= end) {
             int mid = (start + end) / 2;
      
             // If mid is a possible solution, update answer = mid
             if (check(mid, arr, N, K)) {
-                answer = mid;
                 end = mid - 1;
             } else {
                 start = mid + 1;
             }
         }
      
-        return answer;
+        return start;
     }
 };
