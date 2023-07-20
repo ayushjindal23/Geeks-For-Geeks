0e3b436be9d13dc72fffad9b308b42f99eebc671
Ayush Jindal <64908612+ayushjindal23@users.noreply.github.com>
2023-07-20 04:01:14 -0700

    Update Palindrome Check.cpp

--- Strings/Palindrome Check.cpp
+++ Strings/Palindrome Check.cpp
@@ -2,24 +2,27 @@
 #include <string>
 using namespace std;
 
+// Function to check if the given string is a palindrome.
 bool isPalindrome(string str) {
     int left = 0;
     int right = str.length() - 1;
 
+    // Compare characters from the beginning and end of the string until the middle.
     while (left < right) {
         if (str[left] != str[right]) {
-            return false;
+            return false; // If any characters don't match, the string is not a palindrome.
         }
-        left++;
-        right--;
+        left++; // Move left pointer towards right.
+        right--; // Move right pointer towards left.
     }
 
-    return true;
+    return true; // If the loop completes without finding any mismatch, the string is a palindrome.
 }
 
 int main() {
-  
-    string str = "ABCDCBA";
+    string str = "ABCDCBA"; // Sample input string.
+    
+    // Call the isPalindrome function to check if the string is a palindrome.
     if (isPalindrome(str)) {
         cout << "The string is a palindrome." << endl;
     } else {
