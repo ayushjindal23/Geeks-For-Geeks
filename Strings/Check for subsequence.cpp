fb1a3c400c9f7262beffb19d009bb39503a9a4e7
Ayush Jindal <64908612+ayushjindal23@users.noreply.github.com>
2023-07-21 07:13:34 -0700

    Create Check for subsequence.cpp

---
+++ Strings/Check for subsequence.cpp
@@ -0,0 +1,24 @@
+class Solution{
+    public:
+        bool isSubSequence(string A, string B) 
+        {
+            int n = A.length(); // Length of string A
+            int m = B.length(); // Length of string B
+            int i = 0; // Pointer to traverse string A
+        
+            // Iterate through string B
+            for (int j = 0; j < m; j++) {
+                // If the current character of A matches the current character of B
+                // Move the pointer i to the next character in string A
+                if (A[i] == B[j]) {
+                    i++;
+                }
+            }
+        
+            // If the pointer i reaches the end of string A, it means all characters of A
+            // were found in the same order in string B, and A is a subsequence of B.
+            // In this case, return true (1), otherwise return false (0).
+            return (i == n);
+        }
+
+};
