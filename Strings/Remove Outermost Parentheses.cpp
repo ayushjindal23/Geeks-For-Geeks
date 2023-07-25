09de1bb2da68581a7cf3e706fa3950e7337b784a
Ayush Jindal <64908612+ayushjindal23@users.noreply.github.com>
2023-07-25 05:03:55 -0700

    Update Remove Outermost Parentheses.cpp

--- Strings/Remove Outermost Parentheses.cpp
+++ Strings/Remove Outermost Parentheses.cpp
@@ -38,3 +38,37 @@ public:
         return res;
     }
 };
+
+
+// -------------------------------------------------------------------------------- Optimalal Approach -----------------------------------------------------------------------
+
+string removeOuterParentheses(string s) {
+  // Initialize two variables to keep track of the number of opening and closing parentheses.
+  int openCount = 0;
+  int closeCount = 0;
+  string result = "";
+  int start = 0;
+
+  // Loop through each character in the input string.
+  for (int i = 0; i < s.length(); i++) {
+    // If the character is an opening parenthesis, increment the openCount variable.
+    if (s[i] == '(') {
+      openCount++;
+    }
+    // If the character is a closing parenthesis, increment the closeCount variable.
+    else if (s[i] == ')') {
+      closeCount++;
+    }
+
+    // If the openCount variable equals the closeCount variable, it means that a complete substring of parentheses has been encountered.
+    if (openCount == closeCount) {
+      // Add the substring to the output string, without the outermost parentheses.
+      result += s.substr(start + 1, i - start - 1);
+      // Increment the start variable to the index of the first character in the next substring.
+      start = i + 1;
+    }
+  }
+
+  // Return the output string.
+  return result;
+}
